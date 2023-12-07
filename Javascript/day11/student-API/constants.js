f14c86dac04a39fb818a685b57e761c3e2d8221b
trivia-sandyc4136 <sandyc4136@gmail.com>
2023-12-08 00:46:24 +0545

    Constants.JS added

---
+++ Javascript/day11/student-API/constants.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSSAGE="Something went wrong";
+export const INSERT_SUCCESS="Student inserted successfully";
+export const STUDENT_NOT_FOUND="Student not found";
+export const UPDATE_SUCCESS="Student updated";
+export const DELETE_SUCCESS="Student deleted";
