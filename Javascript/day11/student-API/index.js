a02b9c2cb76a685c4b6bf65c09cee165ea6b6cde
trivia-sandyc4136 <sandyc4136@gmail.com>
2023-12-08 01:32:34 +0545

    added dependencies for status codes

--- Javascript/day11/student-API/index.js
+++ Javascript/day11/student-API/index.js
@@ -1,7 +1,8 @@
 import express, { request, response } from "express";
 import mongoose from "mongoose";
 import { Student } from "./StudentModel.js";
-
+import { DELETE_SUCCESS, ERROR_MESSSAGE, INSERT_SUCCESS, STUDENT_NOT_FOUND, UPDATE_SUCCESS } from "./constants.js";
+import { StatusCodes } from "http-status-codes";
 const app=express();
 
 app.use(express.json());
@@ -20,9 +21,9 @@ app.post("/student",async (request,response)=>{
         const reqData=request.body;
         const student=new Student(reqData);
         await student.save();
-        response.send({message:"Student inserted"});
+        response.status(StatusCodes.CREATED).send({message:INSERT_SUCCESS});
     } catch (error) {
-        response.status(500).send({message:"Something went wrong.."});
+        response.status(StatusCodes.INTERNAL_SERVER_ERROR).send({message:ERROR_MESSSAGE});
     }
 });
 
@@ -31,35 +32,35 @@ app.get("/student",async (request,response)=>{
         const students=await Student.find(); // find() function will fetch all the details from the collection 
         response.send({students:students});
     } catch (error) {
-        response.status(500).send({message:"Something went wrong.."});
+        response.status(StatusCodes.INTERNAL_SERVER_ERROR).send({message:ERROR_MESSSAGE});
     }
 });
 
 app.get("/student/:roll", async (request,response)=>{
     try {
         const student=await Student.findOne({roll:request.params.roll});
-        if(student==null) response.status(404).send({"message":"Student not found"});
+        if(student==null) response.status(StatusCodes.NOT_FOUND).send({message:STUDENT_NOT_FOUND});
         response.send({student:student});
     } catch (error) {
-        response.status(500).send({message:"Something went wrong ..."});
+        response.status(StatusCodes.INTERNAL_SERVER_ERROR).send({message:ERROR_MESSSAGE});
     } 
 });
 
 app.delete("/student/:roll",async (request,response)=>{
     try {
         await Student.deleteOne({roll:request.params.roll}); // deleteOne() used to delete single object matching the criteria
-        response.send({message:"Student deleted"});
+        response.send({message:DELETE_SUCCESS});
     } catch (error) {
-        response.status(500).send({message:"Something went wrong..."});
+        response.status(StatusCodes.INTERNAL_SERVER_ERROR).send({message:ERROR_MESSSAGE});
     }
 })
 
 app.put("/student/:roll",async (request,response)=>{
     try {
         await Student.updateOne({roll:request.params.roll},request.body);
-        response.send({message:"Student Updated"});
+        response.send({message:UPDATE_SUCCESS});
     } catch (error) {
-        response.status(500).send({message:"Something went wrong..."});
+        response.status(StatusCodes.INTERNAL_SERVER_ERROR).send({message:ERROR_MESSSAGE});
     }
 })
 app.listen(5940,()=>{
